79bc1a71b6c4ccbe7843f66c9aeb3806f4067d89
kaustubhagarwa <kaustubhagarwal18@gmail.com>
2019-09-06 11:08:39 +0200

    lec2

---
+++ lec2.R
@@ -0,0 +1,22 @@
+# get working directory
+getwd()
+
+# set working directory
+
+setwd("~/Desktop/TU Delft/EPA1315/lecture2")
+
+# read various formats - read.table()
+# separator is a , 
+
+survey_comma = read.table("survey_comma.csv", header = 1,sep = ',')
+survey_sent = read.table("survey_semi.csv",header=1,sep=';')
+survey_tab = read.table("survey_tab.txt",header=1,sep='\t')
+
+install.packages("xlsx")
+library("xlsx")
+survey_xlsx = read.xlsx("survey_xls.xlsx",header=1,sheetIndex=1)
+
+#what the data set contains
+str(survey_tab)
+# more detailed summary
+summary(survey)
